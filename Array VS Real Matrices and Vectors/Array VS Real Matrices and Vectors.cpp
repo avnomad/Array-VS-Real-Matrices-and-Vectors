9cba8d8b8ecb47afce1fc6273e7e36c308de6715
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-23 20:55:12 +0200

    Created a VS2010 project for old code and added a property sheet with optimization flags.
    (Project created using wizard.)
    
    --HG--
    branch : C++/CLI CLR Console Application linked with Native C++ Library

---
+++ Array VS Real Matrices and Vectors/Array VS Real Matrices and Vectors.cpp
@@ -0,0 +1,11 @@
+// Array VS Real Matrices and Vectors.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hello World");
+    return 0;
+}
