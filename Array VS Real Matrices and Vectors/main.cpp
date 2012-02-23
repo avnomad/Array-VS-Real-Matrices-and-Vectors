cf2906fd0efa1488a07b9f648094c35eb0a46758
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-23 20:43:41 +0200

    Added the Array test from the corresponding CLR Console project. Minor other changes.
    Removed some comments.
    Replaced some using directives.
    Removed some unused #includes.
    
    --HG--
    branch : Native C++ Application linked with Native C++ Library

--- Array VS Real Matrices and Vectors/main.cpp
+++ Array VS Real Matrices and Vectors/main.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::endl;
 
 #include <Real Matrices and Vectors.h>
-#include <iomanip>
-#include <time.h>
+#include <Array/Array.h>
 
 using namespace ASL;
+using namespace ASL::DataStructures;
+
+
+double (*sinp)(double) = sin;
+double (*cosp)(double) = cos;
+
+double add1(double x)
+{
+	return 1+x;
+}
+
 
 int main()
 {
+	Array<double> A(25000000); 
+	Array<double> B;
+
+	A = add1;
+	cout<<A.length()<<endl;
+	tic();
+	B = A.mapped(sinp).map(cosp).map(sinp);
+	toc();
+
 	Vector v,u;
 
 	v = C/1/25000000;
-	
+	cout<<length(v)<<endl;
 	tic();
 	u = sin(cos(sin(v)));
-	toc();
-
-	
+	toc();	
 
 	system("PAUSE");
 	return 0;
 } // end fuction main
-
-/*
-	v = linspace(1,4,4);
-	A = hilb(5000);
-
-	tic();
-	A = polyval(v,A);
-	toc();
-
-	532ms optimized vs 723ms matlab		passing to library offers 26.4% reduction of computation time.
-*/
